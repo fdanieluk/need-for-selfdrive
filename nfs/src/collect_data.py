eee444639afe94d87e06201baa94132aa5ccad48
Filip Danieluk <filip.danieluk94@gmail.com>
2020-05-16 19:05:06 +0200

    stylistic changes, removed sleeps, replaced unpause button T with 0

--- nfs/src/collect_data.py
+++ nfs/src/collect_data.py
@@ -1,26 +1,25 @@
-from get_keys import key_check, keys_to_output
-from time import sleep, time
-from screen_grab import grab_screen
-import numpy as np
-from os.path import join
-# from nfs import ROOT
-from datetime import datetime
-from PIL import Image
+"""Main script, grabs screenshots and labels them with keypress and time.
+"""
 from pathlib import Path
+from time import sleep, time
+from datetime import datetime
+
+from PIL import Image
+
+from screen_grab import grab_screen
+from get_keys import key_check, keys_to_output
 
 
 RAW_DATA = Path('..') / 'data' / 'raw'
 
 
 def main():
-    sleep(1)  # to clean last keys from memory
     paused = True
-    training_data = []
 
-    while(True):
+    while True:
         keys = key_check()
 
-        if 'T' in keys:  # press T to start recording
+        if '0' in keys:  # press = to start recording
             if paused:
                 paused = False
                 print('Unpaused!')
@@ -28,31 +27,29 @@ def main():
                 print('Paused!')
                 paused = True
 
-            sleep(1)
-
         if not paused:
             before = time()
+            
             scr = grab_screen()
             keys = key_check()
-            after = time()
 
             _, key_map = keys_to_output(keys)
+
+            # don't save screenshot without any keypress assigned
             if key_map == 'nk':
                 continue
-
-            print(f'It took: {after - before}s, {key_map}')
-
 
             file_name = f'{datetime.now().strftime("%d%m%Y_%H%M%S")}_{key_map}.jpg'
             save_path = RAW_DATA / file_name
 
             img = Image.fromarray(scr, 'RGB')
-            # img.save(save_path)
             img.save(save_path, "JPEG", optimize=True)
 
+            after = time()
+            print(f'It took: {after - before}s, {key_map}')
 
 
-        sleep(1)  # how often it takes
+        sleep(1)  # screenshot frequency
 
 
 if __name__ == '__main__':
